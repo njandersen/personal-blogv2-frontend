0e6df8d2992d7b4ffc042a9c0d571a94e4948804
njandersen <njandersen22@gmail.com>
2022-11-02 21:27:40 -0400

    minor css change

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="mt-4 bg-white border-2 border-cyan-500 p-2 text-xl  shadow-xl shadow-slate-300">
+    <nav className="mt-6 bg-white border-b-2 border-teal-500 p-2 text-xl  shadow-xl shadow-slate-300">
       <ul className="flex flex-row justify-between">
-        <li className="ml-2 px-3 rounded-full border-2 border-cyan-500 bg-white hover:bg-cyan-500 hover:text-white">
+        <li className="ml-2 px-3 rounded-full border-2 border-teal-500 bg-white hover:bg-teal-500 hover:text-white">
           <Link to=".." relative="path">
             FEED
           </Link>
         </li>
-        <li className="ml-2 px-3 rounded-full border-2 border-cyan-500 bg-white hover:bg-cyan-500 hover:text-white">
+        <li className="ml-2 px-3 rounded-full border-2 border-teal-500 bg-white hover:bg-teal-500 hover:text-white">
           <Link to="about">About</Link>
         </li>
-        <li className="ml-2 px-3 rounded-full border-2 border-cyan-500 bg-white hover:bg-cyan-500 hover:text-white">
+        <li className="ml-2 px-3 rounded-full border-2 border-teal-500 bg-white hover:bg-teal-500 hover:text-white">
           <Link to="create">Create Post</Link>
         </li>
-        <li className="ml-2 px-3 rounded-full border-2 border-cyan-500 bg-white hover:bg-cyan-500 hover:text-white">
+        <li className="ml-2 px-3 rounded-full border-2 border-teal-500 bg-white hover:bg-teal-500 hover:text-white">
           <Link to="enter">Sign In</Link>
         </li>
       </ul>
